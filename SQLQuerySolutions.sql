a8948d465524960a18f5f689e58ead25e642595c
Allison Collins <allisonkatecollins@gmail.com>
2019-05-30 18:43:35 -0500

    first 7 queries done

--- SQLQuerySolutions.sql
+++ SQLQuerySolutions.sql
@@ -33,3 +33,26 @@ WHERE Title ='Sales Support Agent';
 --5. unique_invoice_countries.sql: Provide a query showing a unique/distinct list of billing countries from the Invoice table.
 SELECT DISTINCT BillingCountry 
 FROM Invoice;
+
+--6. sales_agent_invoices.sql: Provide a query that shows the invoices associated with each sales agent. 
+--The resultant table should include the Sales Agent's full name.
+SELECT CONCAT(e.FirstName, ' ', e.LastName) AS 'Agent Name',
+	i.InvoiceId,
+	i.BillingAddress,
+	i.BillingCity,
+	i.BillingCountry,
+	i.BillingPostalCode,
+	i.Total
+FROM Customer c
+LEFT JOIN Employee e ON e.EmployeeId = c.SupportRepId
+LEFT JOIN Invoice i ON i.CustomerId = c.CustomerId; 
+--if an employee's Id matches a customer's support rep Id then they are a sales rep; this filters the results to include only sales agents
+
+--7. invoice_totals.sql: Provide a query that shows the Invoice Total, Customer name, Country and Sale Agent name for all invoices and customers.
+SELECT i.Total AS 'Invoice Total',
+	CONCAT(c.FirstName, ' ', c.LastName) AS 'Customer Name',
+	i.BillingCountry,
+	CONCAT(e.FirstName, ' ', e.LastName) AS 'Agent Name'
+FROM Customer c
+LEFT JOIN Employee e ON e.EmployeeId = c.SupportRepId
+LEFT JOIN Invoice i ON i.CustomerId = c.CustomerId;
